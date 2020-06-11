9cc9c8f1894069e7994a757a20ec5030181a2c81
Cloud User <centos@ip-172-31-3-78.eu-west-2.compute.internal>
2020-06-11 16:01:34 +0000

    some changes to workbench, not successful yet

--- Ex4/top.v
+++ Ex4/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #4 
-// Student Name:
-// Date: 
+// Student Name:Juetong Liang
+// Date: 9/6/2020
 //
 //  Description: In this exercise, you need to design an electronic dice, following
 //  the diagram provided in the exercises documentation. The dice rolls as long as
@@ -15,4 +15,29 @@
 //
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
+module dice (
+	input rst,
+	input clk,
+	input button,
+	output reg [2:0] throw
+	);
 
+	always @(posedge clk)
+		begin
+		if (rst==0)
+			begin
+			throw<=3'b1;
+			if (button==1)
+				throw<=throw+3'b1;
+			else
+				throw<=throw;
+			end
+		else
+			begin
+			throw<=3'b0;
+			end
+		end
+endmodule
+
+				
+	
--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -1,9 +1,38 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #4 - Electronic Dice
-// Student Name:
-// Date: 
+// Student Name:Juetong Liang
+// Date: 9/6/2020
 //
 // Description: A testbench module to test Ex4 - Electronic Dice
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+module top_tb();
+
+	reg rst;
+	reg clk;
+	reg button;
+	wire [2:0]throw;
+	
+	parameter clk_period=10;
+		initial 
+			begin
+			clk=1'b0;
+			forever
+			#(clk_period/2)clk=~clk;
+			end
+
+	initial 
+	begin
+	rst=0;
+	button=0;
+	#10
+	button=1;
+	#10
+	button=0;
+	#10
+	rst=0;
+	end
+
+dice top(rst,clk,button,throw);
+endmodule
 
